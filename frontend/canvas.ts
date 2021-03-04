617cdf317c6af509ac98ee832caab8b76b8f5635
Lukas Neumann <lukasneumann93@web.de>
2021-03-04 20:36:19 +0100

    hello PIXI

--- frontend/canvas.ts
+++ frontend/canvas.ts
@@ -1,5 +1,5 @@
+import * as PIXI from 'pixi.js';
 const socketio = require("socket.io-client");
-
 /**
  * Pose type.
  */
@@ -59,23 +59,14 @@ function register_function() {
 /**
  * Sets up the canvas according to screen size.
  */
-function setUpCanvas() {
-    const canvas = <HTMLCanvasElement>document.getElementById("game");
-    const ctx = canvas?.getContext('2d');
-    ctx?.translate(0.5, 0.5);
-
-    // Set display size (vw/vh).
-    var sizeWidth = 100 * window.innerWidth / 100,
-        sizeHeight = 100 * window.innerHeight / 100 || 766;
-
-    //Setting the canvas site and width to be responsive 
-    canvas.width = sizeWidth;
-    canvas.height = sizeHeight;
-    canvas.style.width = sizeWidth.toString();
-    canvas.style.height = sizeHeight.toString();
+function setUpPIXI() {
+    let app = new PIXI.Application({width: 1024, height: 1024, forceCanvas: true});
+    app.renderer.backgroundColor = 0x061639;
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(app.view);
 }
 
-window.onload = () => setUpCanvas();
+ window.onload = () => setUpPIXI();
 
 /**
  * Draws the grid???
@@ -84,65 +75,17 @@ window.onload = () => setUpCanvas();
  * @param cellWidth 
  * @param lineWidth 
  */
-function gridLines(GlobalOffsetX: number, GlobalOffsetY: number, cellWidth = 64, lineWidth = 2) {
-    const offsetX = GlobalOffsetX % cellWidth
-    const offsetY = GlobalOffsetY % cellWidth
-    // const swapColors = offsetX > cellWidth & offsetY > cellWidth
-    const numCellsX = Math.ceil(canvas?.width / cellWidth) + 1
-    const numCellsY = Math.ceil(canvas?.height / cellWidth) + 1
-    if (ctx != undefined) {
-        ctx.fillStyle = "#222222"
-        for (let i = -1; i < numCellsX; i++) {
-            ctx?.fillRect(i * cellWidth - offsetX, 0,
-                lineWidth, canvas.height)
-        }
-        for (let i = -1; i < numCellsY; i++) {
-            ctx?.fillRect(0, i * cellWidth - offsetY,
-                canvas.width, lineWidth)
-        }
-    }
-}
+
 
 /**
  * Draw function ???
  */
-function draw() {
-    if (ctx != undefined && player != undefined) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        const canvasCenter = [canvas.width / 2, canvas.height / 2]
-        const camPos = [player.pose.x + mouseOffset[0] / 2, player.pose.y + mouseOffset[1] / 2]
-        gridLines(camPos[0], camPos[1])
-        ctx.font = "12px Impact"
-        ctx.textAlign = "center"
-
-        if (ctx != undefined && gameState != undefined) {
-            for (let playerIndex in gameState) {
-                let state = gameState[playerIndex];
-                const playerCanvasX = canvasCenter[0] + state.pose.x - camPos[0]
-                const playerCanvasY = canvasCenter[1] + state.pose.y - camPos[1]
-                /**
-                 * @todo: check gameState objects.
-                 **/
-                if (ctx != undefined) {
-                    ctx.fillStyle = state.color;
-                    // draw "Test text" at X = 10 and Y = 30   
-                    ctx.fillText(state.name, playerCanvasX + 20, playerCanvasY - 25);
-                    ctx.fillRect(playerCanvasX,
-                        playerCanvasY,
-                        40,
-                        40);
-                }
-            }
-        }
-    }
-}
 
 var diffPose = new Pose(0, 0, 0);
 const socket = socketio();
 
 // State of this connected player.
 var player: PlayerState | undefined;
-var mouseOffset = [0, 0];
 
 // States of all connected players.
 let gameState: { [index: string]: PlayerState } = {};
@@ -151,69 +94,6 @@ var dirDown = false;
 var dirLeft = false;
 var dirRight = false;
 
-var canvas = <HTMLCanvasElement>document.getElementById('game');
-var ctx = canvas.getContext("2d");
-
-// HTML Button Listeners
-var loginButton = <HTMLCanvasElement>document.getElementById('loginbutton')
-var registerButton = <HTMLCanvasElement>document.getElementById('registerbutton')
-loginButton.addEventListener('click', login_function);
-registerButton.addEventListener('click', register_function);
-
-/**
- * Adds event listeners on mouseover.
- */
-canvas.addEventListener('mouseover', function () {
-    addCanvasEventListeners();
-})
-
-/**
- * Removes event listeners on mouseout.
- */
-canvas.addEventListener('mouseout', function () {
-    removeCanvasEventListeners();
-})
-
-function addCanvasEventListeners() {
-    window.addEventListener('mousemove', mousemoveEventListener);
-    window.addEventListener('keydown', keydownEventListener);
-    window.addEventListener('keyup', keyupEventListener);
-}
-
-function removeCanvasEventListeners() {
-    window.removeEventListener('mousemove', mousemoveEventListener);
-    window.removeEventListener('keydown', keydownEventListener);
-    window.removeEventListener('keyup', keyupEventListener);
-}
-
-function mousemoveEventListener(e: { offsetX: number; offsetY: number; }) {
-    mouseOffset = [e.offsetX - canvas.width / 2, e.offsetY - canvas.height / 2];
-}
-
-
-/**
- * Event listener for keyboard press movement (WASD).
- * @param event Keyboard event this handles.
- */
-function keydownEventListener(event: KeyboardEvent) {
-    event.preventDefault();
-
-    /**
-     * @todo check deprecations.
-     */
-    dirUp = dirUp || (event.key.toLowerCase() == 'w')
-    dirDown = dirDown || (event.key.toLowerCase() == 's')
-    dirLeft = dirLeft || (event.key.toLowerCase() == 'a')
-    dirRight = dirRight || (event.key.toLowerCase() == 'd')
-}
-
-function keyupEventListener(event: KeyboardEvent) {
-    event.preventDefault();
-    dirUp = dirUp && !(event.key.toLowerCase() == 'w')
-    dirDown = dirDown && !(event.key.toLowerCase() == 's')
-    dirLeft = dirLeft && !(event.key.toLowerCase() == 'a')
-    dirRight = dirRight && !(event.key.toLowerCase() == 'd')
-}
 
 function sendUpdate() {
     diffPose.x = 0
@@ -225,7 +105,6 @@ function sendUpdate() {
     socket.emit('keypressed', diffPose);
 }
 
-window.setInterval(draw, 33);
 window.setInterval(sendUpdate, 100);
 
 // New user connects.
